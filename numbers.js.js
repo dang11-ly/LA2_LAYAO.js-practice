002be860f504130632aee2fd77fa9a4e5744c95b
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-23 17:48:43 +0800

    Add files via upload

---
+++ numbers.js.js
@@ -0,0 +1,20 @@
+// Numbers
+var firstFavNum = 11;
+var secondFavNum = 21;
+
+var sumOfall = firstFavNum + secondFavNum;
+var f1Addf2 = firstFavNum + secondFavNum;
+var f1Subtractf2 = firstFavNum - secondFavNum;
+var f1Multiplyf2 = firstFavNum * secondFavNum;
+var f1Dividedf2 = firstFavNum / secondFavNum;
+var f1Modf2 = firstFavNum % secondFavNum;
+var f1ToThePowerf2 = firstFavNum ** secondFavNum;
+
+
+console.log(sumOfall);
+console.log(f1Addf2);
+console.log(f1Subtractf2);
+console.log(f1Multiplyf2);
+console.log(f1Dividedf2);
+console.log(f1Modf2);
+console.log(f1ToThePowerf2);
